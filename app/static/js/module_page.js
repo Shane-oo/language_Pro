737b87e007e354396809cc2f7fe9d85a5612bdf0
Shane <shanemonck11@hotmail.com>
2021-05-03 16:32:44 +0800

    in progress accordion

--- app/static/js/module_page.js
+++ app/static/js/module_page.js
@@ -1,4 +1,5 @@
-window.onload = function () {
+window.onload = function() {
+
 var acc = document.getElementsByClassName("accordion");
 var i;
 console.log(acc)
@@ -13,4 +14,27 @@ for (i = 0; i < acc.length; i++) {
     }
   });
     }
+
+
+
+}
+function move() {
+    var i = 0;
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("module1Bar");
+    console.log(elem)
+    var width = 10;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem.innerHTML = width + "%";
+      }
+    }
+  }
 }
