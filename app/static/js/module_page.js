10ac3f4760849cc756c9cd6305618d52bc2dfc88
Shane <shanemonck11@hotmail.com>
2021-05-12 03:35:31 +0800

    hola done. Template set.

--- app/static/js/module_page.js
+++ app/static/js/module_page.js
@@ -15,55 +15,70 @@ for (i = 0; i < acc.length; i++) {
   });
     }
 }
-// Progress bars inside accordions
+
+
+
+
+
+
+// Progress bars for module
+var width = 0;
 function move() {
-    var i = 0;
-  if (i == 0) {
-    i = 1;
-    var elem = document.getElementById("module1Bar");
+   
+    var elem = document.getElementById("module2Bar");
     console.log(elem)
-    var width = 10;
-    var id = setInterval(frame, 10);
-    function frame() {
-      if (width >= 100) {
-        clearInterval(id);
-        i = 0;
-      } else {
-        width++;
-        elem.style.width = width + "%";
-        elem.innerHTML = width + "%";
-      }
-    }
-  }
-}
-//content next and previous
-
-
-  var counter = 1;
-  $('body').on('click', '.next', function() { 
-    $('.content').hide();
-
-    counter++;
-    $('#content-'+counter+'').show();
-
-    if(counter>1){
-      $('.back').show();
-    }
-    //when module finised
-    if(counter>3){
-      $('.wordLearn').hide();
-      $('.end').show();
-    };
-  });
-  //clicked previous
-  $('body').on('click','.back',function(){
-    counter--;
-    $('.content').hide()
-    var id = counter;
-    $('#content-'+id).show();
-      if(counter<2){
-        $('.back').hide();
-      }
-
-    });
+   
   
+      if (width<100) {
+        width+=25
+        elem.style.width = width + "%";
+        elem.innerHTML = width*1 + "%";
+      }
+}
+function unmove(){
+     
+  var elem = document.getElementById("module2Bar");
+  console.log(elem)
+    if (width<100) {
+      width-=25;
+      elem.style.width = width + "%";
+      elem.innerHTML = width*1 + "%";
+    }
+  
+}
+
+
+//content next and previous
+var counter = 1;
+$('body').on('click', '.next', function() { 
+  $('.content').hide();
+
+  counter++;
+  $('#content-'+counter+'').show();
+
+  if(counter>1){
+    $('.back').show();
+  }
+  //when module finised
+  if(counter>4){
+    $('.wordLearn').hide();
+    $('.end').show();
+  };
+});
+//clicked previous
+$('body').on('click','.back',function(){
+  counter--;
+  $('.content').hide()
+  var id = counter;
+  $('#content-'+id).show();
+    if(counter<2){
+      $('.back').hide();
+    }
+
+});
+
+//play sounds
+var hola = new Audio("/static/sounds/hola.m4a"); 
+var buenosDias = new Audio("/static/sounds/buenosDias.m4a");
+var buenasTardes = new Audio("/static/sounds/buenasTardes.m4a");
+var buenasNoches = new Audio("/static/sounds/buenasNoches.m4a");
