aee89e81e43923b01c20b8c85a293e91ac51ac95
yuhshin-00 <82132833+yuhshin-00@users.noreply.github.com>
2021-05-11 16:42:04 +0800

    home page js

---
+++ app/static/js/homepage script.js
@@ -0,0 +1,12 @@
+const navslide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('nav-links');
+
+    burger.addEventListener('click', ()=> {
+        nav.classlist.toggle('nav-active');
+    })
+}
+
+const app = ()=> {
+    navSlide();
+}
