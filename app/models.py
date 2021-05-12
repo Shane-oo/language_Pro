16b63df809efab6be01d85a25ff14d4286bc79c4
Faaaah <nuri.0316@gmail.com>
2021-05-12 23:57:34 +0800

    Updates made on Login Page
    updates made on :
     routes
    
    created new py files :
    1. models
    2. forms
    3.config

---
+++ app/models.py
@@ -0,0 +1,21 @@
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+#from flask_login import UserMixin 
+
+#creating user database model - UserMixin : mixin class that includes generic implementations that are appropriate for most user model classes. 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128)) #security - user password will not be stored in the db if the database ever becomes compromised, the attackers will have access to the passwords, and that could be devastating for users. Instead of writing the passwords directly, I'm going to write password hashes, which greatly improve security. 
+    firstname = db.Column(db.String(64))
+    lastname = db.Column(db.String(64)) 
+
+    def __repr__(self):
+        return '<User {}>'.format(self.email)  
+
+    #password hashing 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)  
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,11 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for, request
+from app import app, db
+from app.forms import LoginForm
+#from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
+from werkzeug.urls import url_parse
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
@@ -7,13 +13,31 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/loginPage')
+@app.route('/loginPage', methods=['GET', 'POST'])
 def loginPage():
-    return render_template('loginPage.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+    return render_template('loginFinal.html', form=form)
+
+
+@app.route('/loginTest', methods=['GET', 'POST'])
+def loginTest():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.email.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('loginTest.html', title='Sign In', form=form)
+
 
 @app.route('/forgetPass')
 def forgetPass():
     return render_template('forgetPass.html')
+
 
 @app.route('/signupPage')
 def signupPage():
