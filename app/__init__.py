551d61f608b27d8e6704711ee53f59b82b03f50e
yuhshin-00 <yuhshinlim0808@gmail.com>
2021-05-16 03:20:14 +0800

    Unit Testing
    Tested login and hash password

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -17,3 +18,4 @@ login.login_view = 'loginTest' # <- 'login' is the page name
 
 
 from app import routes, models
+app.run(debug=True)
