043f2956bfddbbc6f54d747058a46cfdf3d277f8
yuhshin-00 <yuhshinlim0808@gmail.com>
2021-05-16 03:44:30 +0800

    Revert "junk file"
    This reverts commit 6e51d79f8d354cf3a4c9e132086667c8909911bb.

--- app/routes.py
+++ app/routes.py
@@ -77,18 +77,11 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
-<<<<<<< HEAD
-=======
-@app.route('/loginPage')
-def loginPage():
-    return render_template('loginFinal.html')
->>>>>>> main
 
 @app.route('/forgetPass')
 def forgetPass():
     return render_template('forgetPass.html')
 
-<<<<<<< HEAD
 # @app.route('/signupPage2', methods=['GET', 'POST'])
 # def signupPage2():
 #     if current_user.is_authenticated:
@@ -140,16 +133,3 @@ def user():
     return render_template('user.html', name=current_user.firstname)
 
 
-=======
-@app.route('/signupPage')
-def signupPage():
-    return render_template('signupFinal.html')
-
-@app.route('/module_page')
-def moudule_page():
-    return render_template('module_page.html')
-
-@app.route('/test1_page')
-def test1_page():
-    return render_template('learningQuizzes/test1_page.html')
->>>>>>> main
