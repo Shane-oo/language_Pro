e7ec9b2e6c0fa200ef42531d66f654c896fdc8a4
Shane <shanemonck11@hotmail.com>
2021-05-17 00:26:01 +0800

    join All files 1.0

--- app/routes.py
+++ app/routes.py
@@ -1,54 +1,32 @@
 from flask import render_template,flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
 from app.email import send_password_reset_email
 
-
-
 @app.route('/')
-@app.route('/index2')
-def index2():
-    return render_template('index2.html')
-
 @app.route('/index')
 def index():
     return render_template('index.html')
 
+
+
+
+
+#module
 @app.route('/home')
 @login_required  # to protect some pages nd only allow access to authenticated user
 def home():
     return render_template("home.html", title='Main Page')
 
 
-# @app.route('/loginTest', methods=['GET', 'POST'])
-# def loginTest():
-#     form = LoginForm()
-#     if request.method == 'POST':
-#         if form.validate_on_submit():
-#             user = User.query.filter_by(email=form.email.data).first()
-#             if user is None or not user.check_password(form.password.data):
-#                 flash('Invalid username or password')
-#                 return redirect(url_for('loginTest'))
-#             login_user(user, remember=form.remember_me.data)
-#             next_page = request.args.get('next')
 
-#             if not next_page or url_parse(next_page).netloc != '':
-#                 # page user enters after log in
-#                 next_page = url_for('home')
-#             return redirect(next_page)
 
-#     # check if user is logged in yet - false if logged in and true if isn't
-#     if current_user.is_authenticated:
-#         return redirect(url_for('home'))
-#     return render_template('loginTest.html', form=form)
-
-@app.route('/login_Final_2', methods=['GET', 'POST'])
-def login_Final_2():
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     if request.method == 'GET':
-        return render_template('login_Final_2.html')
+        return render_template('login.html')
     
     else : 
         email = request.form.get('email')
@@ -57,13 +35,13 @@ def login_Final_2():
 
         if user is None or not user.check_password(password):
             flash('Invalid username or password','danger')
-            return redirect(url_for('login_Final_2'))
+            return redirect(url_for('login'))
         login_user(user)
         next_page = request.args.get('next')
 
         if not next_page or url_parse(next_page).netloc != '':
             # page user enters after log in
-            next_page = url_for('home')
+            next_page = url_for('index')
             return redirect(next_page)
 
     # check if user is logged in yet - false if logged in and true if isn't
@@ -74,7 +52,7 @@ def login_Final_2():
 @app.route('/forgot_pass', methods =['GET', 'POST'])
 def forgot_pass():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('home'))
 
     if request.method == 'POST' : 
         email = request.form.get('email')
@@ -83,7 +61,7 @@ def forgot_pass():
         if user : 
             send_password_reset_email(user)
             flash('Check your email for the instructions to reset your password', 'success')
-            return redirect(url_for('login_Final_2'))
+            return redirect(url_for('login'))
     
     return render_template('forgot_pass.html')
 
@@ -101,7 +79,7 @@ def pass_request(token, new_password):
         db.session.commit()
 
         flash('Your password has been reset!', 'success')
-        return redirect(url_for('login_Final_2'))
+        return redirect(url_for('login'))
 
     return render_template('pass_request.html')
 
@@ -111,29 +89,10 @@ def logout():
     return redirect(url_for('index'))
 
 
-# @app.route('/forgetPass')
-# def forgetPass():
 
-#     return render_template('forgetPass.html')
 
-# @app.route('/signupPage2', methods=['GET', 'POST'])
-# def signupPage2():
-#     if current_user.is_authenticated:
-#         return redirect(url_for('home'))
-#     form = RegistrationForm()
-
-#     #adding user to db 
-#     if form.validate_on_submit():
-#         user = User(email=form.email.data, firstname = form.firstname.data, lastname=form.lastname.data)
-#         user.set_password(form.password.data)
-#         db.session.add(user)
-#         db.session.commit()
-#         flash('Congratulations, you are now a registered user!')
-#         return redirect(url_for('loginTest'))
-#     return render_template('signupPage2.html', title='Register', form=form)
-
-@app.route('/sign_up_3', methods=['GET', 'POST'])
-def sign_up_3():
+@app.route('/signUp', methods=['GET', 'POST'])
+def signUp():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
 
@@ -148,7 +107,7 @@ def sign_up_3():
 
         if user : 
             flash('Email address already exists!','warning')
-            return redirect(url_for('login_Final_2'))
+            return redirect(url_for('login'))
         
         new_user = User(firstname = firstname, lastname=lastname, email=email)
         new_user.set_password(password)
@@ -156,14 +115,9 @@ def sign_up_3():
         db.session.add(new_user)
         db.session.commit()
         flash('Congratulations, you are now a registered user!', 'success')
-        return redirect(url_for('login_Final_2'))
-    return render_template('sign_up_3.html', title='Register')
+        return redirect(url_for('login'))
+    return render_template('signUp.html', title='Register')
 
     
-#user profile view 
-@app.route('/user')
-@login_required  # protects a view function against anonymous users
-def user():
-    return render_template('user.html', name=current_user.firstname)
 
 
